d1a5163770bfb411bd9e7896ba63f98e174e658c
fatimahk1234 <88849209+fatimahk1234@users.noreply.github.com>
2022-12-24 09:45:55 -0800

    Add files via upload

---
+++ PROC112.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+from_dir = "C:/Users/fatim/Downloads/Images"
+to_dir = "C:/Users/fatim/Desktop/DImg"
+list_of_files = os.listdir(from_dir)
+for file_name in list_of_files:
+    name, extension = os.path.splitext(file_name)
+
+    if extension == '':
+        continue
+    if extension in ['.pdf','.txt','.doc','.docx']:
+
+        path1 = from_dir +'/' + file_name
+        path2 = to_dir + '/' + 'images'
+        path3 = to_dir +'/' + 'images' + '/' + file_name
+
+        if os.path.exists(path2):
+            print("Moving"+file_name)
+            shutil.move(path1, path3)
+
+        else:
+            os.makedirs(path2)
+            print("Making the directories")
+            print("Movinng"+file_name)
+            shutil.move(path1, path3)
